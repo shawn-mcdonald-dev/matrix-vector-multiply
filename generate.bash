d623e62da5274dfd0acf9dad44f1d6391b4e1daa
Shawn McDonald <shawnmcdon01@gmail.com>
2026-03-17 22:59:47 -0400

    fixed sh scripts

--- generate.bash
+++ generate.bash
@@ -3,7 +3,6 @@
 N_VALUES=(256 512 1024 2048 4096 8192)
 
 for N in "${N_VALUES[@]}"; do
-    echo "Generating N=$N"
     ./make_matrix A_$N.bin $N $N
-    ./make_matrix X_$N.bin $N 1
+    ./make_matrix B_$N.bin $N $N
 done
--- run_p1.sh
+++ run_p1.sh
@@ -17,43 +17,15 @@ P=1
 
 N_VALUES=(256 512 1024 2048 4096 8192)
 
-RESULT_FILE="results_p${P}.csv"
-
-echo "N,P,Tp_overall,Speedup_overall,Efficiency_overall,Tp_work,Speedup_work,Efficiency_work" > $RESULT_FILE
-
-T1_overall=0
-T1_work=0
+echo "N,P,Tp_overall,Tp_work" > results_p${P}.csv
 
 for N in "${N_VALUES[@]}"; do
 
-    echo "Running N=$N P=$P"
+    OUTPUT=$(./pth_matrix_matrix A_${N}.bin B_${N}.bin C.bin $P)
 
-    OUTPUT=$(./pth_matrix_vector A_${N}.bin X_${N}.bin Y.bin $P)
+    TP_OVERALL=$(echo "$OUTPUT" | grep "Overall" | awk '{print $3}')
+    TP_WORK=$(echo "$OUTPUT" | grep "Compute" | awk '{print $3}')
 
-    TP_OVERALL=$(echo "$OUTPUT" | grep "Overall time" | awk '{print $3}')
-    TP_WORK=$(echo "$OUTPUT" | grep "Compute time" | awk '{print $3}')
-
-    TP_OVERALL=$(printf "%.6f" $TP_OVERALL)
-    TP_WORK=$(printf "%.6f" $TP_WORK)
-
-    if [ "$P" -eq 1 ]; then
-        SPEEDUP_overall=1.0
-        EFFICIENCY_overall=1.0
-        SPEEDUP_work=1.0
-        EFFICIENCY_work=1.0
-
-        T1_overall=$TP_OVERALL
-        T1_work=$TP_WORK
-    else
-        # NOTE: You’ll merge with P=1 later
-        SPEEDUP_overall=0
-        EFFICIENCY_overall=0
-        SPEEDUP_work=0
-        EFFICIENCY_work=0
-    fi
-
-    echo "$N,$P,$TP_OVERALL,$SPEEDUP_overall,$EFFICIENCY_overall,$TP_WORK,$SPEEDUP_work,$EFFICIENCY_work" >> $RESULT_FILE
+    echo "$N,$P,$TP_OVERALL,$TP_WORK" >> results_p${P}.csv
 
 done
-
-echo "Done P=$P"
